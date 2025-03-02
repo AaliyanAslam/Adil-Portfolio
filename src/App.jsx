0947989af4fd00bccad652b39cc7dee9076073ee
Adil Aslam <aaliyanaslam22@gmail.com>
2025-03-02 16:16:48 +0500

    🔥 added akills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import React from "react";
 import { NavbarSec } from "./components/NavbarSec";
 import { Services } from "./components/Services";
+import {Skills} from "./components/Skills";
 export default function App() {
   return (
     <>
       <NavbarSec />
       <Services />
+      <Skills />
      
 
       
